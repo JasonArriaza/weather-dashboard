00b682da7081f07b7c6353fa06818a0c215eff91
Jason <JasonArriaza10@gnail.com>
2024-01-20 21:36:10 -0600

    added all the functions in getting the lat and lon of a city and displaying it in the console

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,6 @@
-var apiKey = '';
+var apiKey = 'a2000ba3cceed20cb5cc94de29283744';
 var listOfCities = '';
+var searchedCity = '';
 //function to grab the input from the search bar
 function getSearchInput() {
     var searchInput = document.getElementById('cityInput');
@@ -7,7 +8,7 @@ function getSearchInput() {
     console.log('User Input', userInput);
     searchCity(userInput);
 }
-//function to find city and use its lat and long to find weather info
+//function to search fot the cities with the given name
 function searchCity() {
     console.log('Search city funciton is receiving the value')
     var apiUrlCityInfo = `http://api.openweathermap.org/geo/1.0/direct?q=${userInput},&limit=5&appid=${apiKey}`;
@@ -19,20 +20,37 @@ function searchCity() {
         } else if (response.ok) {
             var listOfCities = response.json();
             console.log('Here is the list of cities:', listOfCities);
-            extractLatLong(listOfCities);
             return listOfCities;
+
         }
     })
+    .then(cityData => {
+        listOfCities = cityData;
+        console.log('Here is the list of cities:', listOfCities);
+        extractFirstCity();
+    })
+    }
+
+//function to extract the first city in the array
+function extractFirstCity() {
+    if (listOfCities.length > 0) {
+        console.log('First city:', listOfCities[0]);
+        var searchedCity = listOfCities[0];
+        extractLatLong(searchedCity);
+        return searchedCity;
+    } else {
+        console.log('No cities found');
+    }
 }
-
-//function to extract the lat and lon from the fist city in the array
-function extractLatLong() {
-    console.log(listOfCities[0]);
-}
-
-
-
- //function to use lat and long find weather info
+ //function to extract the lat and lon in the chosen city
+ function extractLatLong(searchedCity){
+    if(searchedCity){
+        if(searchedCity){
+            console.log('Lat:', searchedCity.lat);
+            console.log('Lon:', searchedCity.lon);
+        }
+    }
+ }
 // function cityWeather() {
 // }
 
