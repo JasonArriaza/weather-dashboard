8979c7df853152d4e5205ed2de66bb432db900b6
JasonArriaza <JasonArriaza10@gmail.com>
2024-01-27 19:14:24 -0600

    formatted the document

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,7 +5,7 @@ var searchedCity = '';
 
 function getCurrentDate() {
     var currentDate = new Date();
-    
+
     var months = [
         "January", "February", "March", "April", "May", "June",
         "July", "August", "September", "October", "November", "December"
